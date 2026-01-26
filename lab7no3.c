03bfcc2bd81fc166860b113aaf294059098d68a8
6804062610215 <s6804062610215@email.kmutnb.ac.th>
2026-01-26 11:48:49 +0700

    Create lab7no3.c
    สร้าง array 1 มิติ ขนาด N โดยให้มีสมาชิกจำนวนเต็มระหว่าง 1-100 แต่เลขยังซ้ำกัน

---
+++ lab7no3.c
@@ -0,0 +1,23 @@
+#include<stdio.h> //printf() , scanf()
+#include<math.h>  // ex. Math functions
+#include<ctype.h> // ex. Character manipulation functions
+
+
+int randomInt(int min, int max);
+
+int main(){
+	int arr[100], i, n;
+	scanf("%d", &n);
+	for(i = 0;i < n;i++){
+		arr[i] = randomInt(1, 100);
+	}
+	
+	for(i = 0;i < n;i++){
+		printf("%d\n", arr[i]);
+	}
+	
+}
+
+int randomInt(int min, int max){
+	return((rand() % (max - min + 1)) + min);
+}
